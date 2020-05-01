d7065d04c693b2cbc0c3141e3a4e2387281f300d
Markus Dormann <Markus@Dormann.it>
2020-05-02 00:16:00 +0200

    refactoring & nuget update

--- ISSFlyBy/ISSFlyBy/ISSFlyBy/ViewModel/AboutViewModel.cs
+++ ISSFlyBy/ISSFlyBy/ISSFlyBy/ViewModel/AboutViewModel.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Windows.Input;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace ISSFlyBy.ViewModels
@@ -11,7 +11,7 @@ namespace ISSFlyBy.ViewModels
         {
             Title = "About";
 
-            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+            OpenWebCommand = new Command(async () => await Launcher.TryOpenAsync("https://xamarin.com/platform"));;
             //<div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
             //<div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
         }
---
+++ ISSFlyBy/ISSFlyBy/ISSFlyBy/Views/ISSLiveView_Tablet.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ISSFlyBy.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ISSLiveView_Tablet : MasterDetailPage
+    {
+        public ISSLiveView_Tablet()
+        {
+            InitializeComponent();
+        }
+    }
+}
